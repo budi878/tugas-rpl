0ecbd6ddd4b50feb2658233c2adf5b68a0a90078
budi878 <ibnoeruff@gmail.com>
2024-11-04 12:30:12 +0700

    selesai

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index()
+    {   $prodcts = Product::all();
+        return view('products.index', ['products'=> $prodcts]);
+    }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function store(Request $request)
+    {
+        // Validate the request data
+        $data = $request->validate([
+            'name' => 'required',
+            'qty' => 'required|numeric',
+            'price' => 'required|numeric|regex:/^\d+(\.\d{1,2})?$/',  // updated validation for decimal price
+            'description' => 'nullable',
+        ]);
+
+        // Create the product and redirect
+         Product::create($data);
+
+        return redirect(route('product.index'));
+    }
+    public function edit(Product $product){
+        return view('products.edit', ['product' => $product]);
+        
+    }
+    public function update(Product $product, Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'qty' => 'required|numeric',
+            'price' => 'required|decimal:0,2',
+            'description' => 'nullable'
+        ]);
+
+        $product->update($data);
+
+        return redirect(route('product.index'))->with('success', 'Product Updated Succesfully');
+    }
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return redirect(route('product.index'))->with('success', 'Product Deleted Succesfully');
+    }
+}
